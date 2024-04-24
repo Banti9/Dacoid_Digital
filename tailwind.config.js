690e04d4395043b8bbc5e9736cab42a854a6b6fb
Banti9 <banti2021cs130@abesit.edu.in>
2024-04-24 16:40:01 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/*.html"],
+  content: [
+    './src/**/*.html',
+    // Add other file paths if necessary (e.g., './src/**/*.js')
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
